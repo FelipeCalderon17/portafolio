f0c8d318c7c21018bf54b310329095ec209188a2
FelipeCalderon17 <calderonfelipe017@gmail.com>
2023-11-28 13:25:39 -0500

    Fin

--- front/src/components/SpecialtiesList.jsx
+++ front/src/components/SpecialtiesList.jsx
@@ -4,8 +4,9 @@ import { educations } from "../constants/data";
 import HelperForm from "../helpers/HelperForm";
 import { Global } from "../helpers/Global";
 
-export default function SpecialtiesList() {
+export default function SpecialtiesList({ studies, setStudies }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [data, setData] = useState({});
 
   const openModal = () => {
     setIsOpen(true);
@@ -13,109 +14,129 @@ export default function SpecialtiesList() {
 
   const closeModal = () => {
     setIsOpen(false);
-    cambiar({});
   };
-  const [data, setData] = useState({});
+
   const [idEdit, setIdEdit] = useState(-1); //Esta en -1 para que no muestre la modal directamente
 
-  const { form, cambiar } = HelperForm({});
-
-  const llenarData = (id) => {
-    const education = educations.find(
-      (edu) => edu.id.toString() === id.toString()
-    );
-
-    if (education) {
-      const datos = {
-        id: education.id,
-        tipo: education.tipo,
-        detalle: education.detalle,
-        notas: education.notas,
-      };
-      setData(datos);
-      openModal();
-      cambiar({});
-    }
-  };
+  const { form } = HelperForm({});
 
   const editarEstudio = async (e) => {
     e.preventDefault();
-    let nuevoPerfil = form;
-    /* console.log(nuevoPerfil); */
-    //guardar en la api
-    const request = await fetch(Global.url + "/estudio/editar/" + idEdit, {
-      method: "PUT",
-      body: JSON.stringify(nuevoPerfil),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const dataCons = await request.json();
-    /* console.log(dataCons); */
+    let tipo = document.querySelector("#tipoEdit");
+    let detalle = document.querySelector("#detalleEdit");
+    let fechaFin = document.querySelector("#fechaFinEdit");
+    let notas = document.querySelector("#notasEdit");
+
+    try {
+      const request = await fetch(Global.url + "/estudio/editar/" + idEdit, {
+        method: "PUT",
+        body: JSON.stringify({
+          tipo: tipo.value,
+          detalle: detalle.value,
+          fechaFin: fechaFin.value,
+          notas: notas.value,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const dataCons = await request.json();
+      console.log("Datos actualizados: ", dataCons); // Verifica los datos recibidos
+
+      // Actualiza el estado después de la edición exitosa
+      closeModal(); // Cierra la modal después de editar
+      // Reinicia los valores a vacío o a un estado inicial según sea necesario
+      setData({});
+      setIdEdit(-1);
+      // También podrías recargar los datos de educación después de la edición para reflejar los cambios inmediatamente
+      // Ejecutar nuevamente la solicitud para obtener los datos actualizados
+      const response = await fetch(Global.url + "/estudio/listar");
+      const data = await response.json();
+
+      // Establecer los datos de educación en el estado
+      setStudies(data.resultado);
+    } catch (error) {
+      console.error("Error editing education: ", error);
+    }
+  };
+
+  const eliminarEstudio = async (idBorrar) => {
+    try {
+      const request = await fetch(Global.url + "/estudio/borrar/" + idBorrar, {
+        method: "DELETE",
+      });
+
+      const dataCons = await request.json();
+      setData({});
+
+      // También podrías recargar los datos de educación después de la edición para reflejar los cambios inmediatamente
+      // Ejecutar nuevamente la solicitud para obtener los datos actualizados
+      const response = await fetch(Global.url + "/estudio/listar");
+      const data = await response.json();
+
+      // Establecer los datos de educación en el estado
+      setStudies(data.resultado);
+    } catch (error) {
+      console.error("Error eliminando education: ", error);
+    }
   };
 
   useEffect(() => {
-    // Llama a llenarData solo cuando cambie el ID
-    llenarData(idEdit);
-  }, [idEdit]);
+    async function fetchAllEducationData() {
+      try {
+        const response = await fetch(Global.url + "/estudio/listar");
+        const data = await response.json();
+
+        // Establecer los datos de educación en el estado
+        setStudies(data.resultado);
+      } catch (error) {
+        console.error("Error fetching all education data: ", error);
+      }
+    }
+
+    fetchAllEducationData();
+  }, []);
   return (
     <div className="w-[100%] grid justify-items-center items-stretch justify-center gap-10 sm:gap-9 md:gap-10 grid-cols-[repeat(auto-fit,minmax(250px,250px))] sm:grid-cols-[repeat(auto-fit,minmax(280px,280px))] md:grid-cols-[repeat(auto-fit,minmax(250px,250px))] lg:grid-cols-[repeat(auto-fit,minmax(215px,215px))] row-auto">
-      {educations.map((education, id) => (
-        <>
-          <div
-            className=" flex flex-col flex-nowrap items-center justify-center gap-7 glassy-screen !w-[350px] !min-h-[300px] !p-5 sm:!w-[250px] sm:!min-h-[200px] sm:!p-4 md:!w-[250px] md:!min-h-[200px] md:!p-5 lg:!w-[250px] lg:!min-h-[200px] lg:!p-7"
-            key={id}
+      {studies.map((education, id) => (
+        <div key={id}>
+          <p>Tipo: {education.tipo}</p>
+          <p>Detalle: {education.detalle}</p>
+          <p>Fecha Fin: {new Date(education.fechaFin).toLocaleDateString()}</p>
+          <p>Notas: {education.notas}</p>
+          <button
+            onClick={() => {
+              openModal(); // Abre la modal al hacer clic en el botón de editar
+              setIdEdit(education._id); // Establece el ID para editar
+              setData({
+                id: education._id,
+                tipo: education.tipo,
+                detalle: education.detalle,
+                notas: education.notas,
+              });
+            }}
           >
-            <div className="text-center space-y-8 sm:text-left">
-              <div className="space-y-4">
-                <p className="text-slate-500  font-medium">
-                  <span className="text-white">Tipo de educación:</span>{" "}
-                  {education.tipo}
-                </p>
-                <p className="text-slate-500  font-medium">
-                  <span className="text-white">Fecha Fin:</span>{" "}
-                  {education.fechaFin.getFullYear()} /{" "}
-                  {education.fechaFin.getMonth()}
-                </p>
-                <p className="text-slate-500  font-medium">
-                  <span className="text-white">Notas:</span> {education.notas}
-                </p>
-                <p className="text-slate-500  font-medium">
-                  <span className="text-white">Detalle:</span>{" "}
-                  {education.detalle}
-                </p>
-              </div>
-              <div className="flex items-center justify-center">
-                <button
-                  onClick={() => {
-                    llenarData(education.id);
-                  }}
-                  className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 glassy-icon m-3"
-                  type="button"
-                >
-                  Editar
-                </button>
-                <button type="reset" className="glassy-icon m-3">
-                  Eliminar
-                </button>
-              </div>
-            </div>
-          </div>
-        </>
+            Editar
+          </button>
+          <br></br>
+          <button
+            onClick={() => {
+              alert(education._id);
+              eliminarEstudio(education._id);
+            }}
+          >
+            Eliminar
+          </button>
+        </div>
       ))}
       {/* Modal */}
       {isOpen && (
         <div className="fixed inset-0 w-full h-full flex items-center justify-center ">
-          <div
-            className="absolute inset-0  bg-black opacity-75"
-            onClick={closeModal}
-          ></div>
+          <div className="absolute inset-0  bg-black opacity-75" onClick={closeModal}></div>
           <div className=" p-8 rounded shadow-lg z-20 bg-modal">
             {/* Contenido de la modal */}
-            <form
-              onSubmit={editarEstudio}
-              className="w-[100%] flex flex-col flex-nowrap items-center gap-7"
-            >
+            <form onSubmit={editarEstudio} className="w-[100%] flex flex-col flex-nowrap items-center gap-7">
               <h3 className="text-xl font-semibold mb-4">Editar Estudio</h3>
               <label className="max-w-[30em] w-[100%]" hidden>
                 id
@@ -127,7 +148,6 @@ export default function SpecialtiesList() {
                   placeholder=""
                   value={data.id}
                   required
-                  onChange={cambiar}
                 />
               </label>
               <label className="max-w-[30em] w-[100%]">
@@ -138,7 +158,6 @@ export default function SpecialtiesList() {
                   name="tipoEdit"
                   className="mt-3 px-5 py-4 bg-transparent border-2 border-solid border-neutGray-500 rounded-[5px] max-w-[30em] w-[100%] backdrop-blur-[5px]"
                   defaultValue={data.tipo}
-                  onChange={cambiar}
                 />
               </label>
               <label className="max-w-[30em] w-[100%]">
@@ -151,7 +170,6 @@ export default function SpecialtiesList() {
                   placeholder=""
                   defaultValue={data.detalle}
                   required
-                  onChange={cambiar}
                 />
               </label>
               <label className="max-w-[30em] w-[100%]">
@@ -163,7 +181,6 @@ export default function SpecialtiesList() {
                   className="mt-3 px-5 py-4 bg-transparent border-2 border-solid border-neutGray-500 rounded-[5px] max-w-[30em] w-[100%] backdrop-blur-[5px]"
                   placeholder=""
                   required
-                  onChange={cambiar}
                 />
               </label>
               <label className="max-w-[30em] w-[100%]">
@@ -175,22 +192,15 @@ export default function SpecialtiesList() {
                   className="mt-3 px-5 py-4 bg-transparent border-2 border-solid border-neutGray-500 rounded-[5px] max-w-[30em] w-[100%] backdrop-blur-[5px]"
                   placeholder=""
                   required
-                  onChange={cambiar}
                   defaultValue={data.notas}
                 />
               </label>
               {/* Botón para cerrar la modal */}
               <div className="flex items-center justify-center">
-                <button
-                  type="submit"
-                  className="mt-4  bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded glassy-icon m-3"
-                >
+                <button type="submit" className="mt-4  bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded glassy-icon m-3">
                   Editar
                 </button>
-                <button
-                  onClick={closeModal}
-                  className="mt-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded glassy-icon m-3"
-                >
+                <button onClick={closeModal} className="mt-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded glassy-icon m-3">
                   Cancelar
                 </button>
               </div>
--- front/src/pages/Services.jsx
+++ front/src/pages/Services.jsx
@@ -8,23 +8,28 @@ import { Global } from "../helpers/Global";
 
 export default function Services() {
   const { form, cambiar } = HelperForm({});
+  const [studies, setStudies] = useState([]);
 
   const agregarEstudio = async (e) => {
     e.preventDefault();
     let nuevoPerfil = form;
-    /* console.log(nuevoPerfil); */
-    //guardar en la api
-    const request = await fetch(Global.url + "/estudio/registrar", {
-      method: "POST",
-      body: JSON.stringify(nuevoPerfil),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const dataCons = await request.json();
-    /* console.log(dataCons); */
+    try {
+      const request = await fetch(Global.url + "/estudio/registrar", {
+        method: "POST",
+        body: JSON.stringify(nuevoPerfil),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const dataCons = await request.json();
 
-    cambiar({});
+      // Actualizar la lista de estudios después de agregar uno nuevo
+      const response = await fetch(Global.url + "/estudio/listar");
+      const data = await response.json();
+      setStudies(data.resultado); // Actualizar la lista de estudios con los nuevos datos
+    } catch (error) {
+      console.error("Error adding new study: ", error);
+    }
   };
 
   return (
@@ -32,10 +37,7 @@ export default function Services() {
       <section className="w-[100%] flex flex-col flex-nowrap items-center justify-center gap-7 px-mobileBound sm:px-8 sm:gap-8 md:gap-9 lg:gap-10">
         <h2 className="text-center">Agregar Estudio</h2>
         {/* <ServiceList services={generalServices} /> */}
-        <form
-          onSubmit={agregarEstudio}
-          className="w-[100%] flex flex-col flex-nowrap items-center gap-7"
-        >
+        <form onSubmit={agregarEstudio} className="w-[100%] flex flex-col flex-nowrap items-center gap-7">
           <label className="max-w-[30em] w-[100%]">
             Tipo
             <input
@@ -97,7 +99,7 @@ export default function Services() {
 
       <section className="w-[100%] flex flex-col flex-nowrap items-center justify-center gap-7 px-mobileBound sm:px-8 sm:gap-8 md:gap-9 lg:gap-10">
         <h2 className="text-center">Estudios</h2>
-        <SpecialtiesList />
+        <SpecialtiesList studies={studies} setStudies={setStudies} />
       </section>
     </>
   );
