2e05820813dd25f228df25b32a57fa0f2389d246
StivenHerrera <stiven3722@gmail.com>
2023-11-29 16:40:31 -0500

    card

--- front/src/components/Projects.jsx
+++ front/src/components/Projects.jsx
@@ -72,17 +72,27 @@ export default function Projects() {
   }, []);
   return (
     <>
-      <ul className="max-w-[1000px] w-auto flex flex-col flex-nowrap items-start justify-center gap-10 md:flex-row md:flex-wrap md:gap-11">
+      <ul className="max-w-[1500px] w-auto flex flex-col flex-nowrap items-start justify-center gap-10 md:flex-row md:flex-wrap md:gap-11">
         {projectList.map((project, id) => (
           <li
             key={id}
-            className=" flex flex-col flex-nowrap items-center justify-center gap-7 glassy-screen !w-[350px] !min-h-[300px] !p-5 sm:!w-[250px] sm:!min-h-[200px] sm:!p-4 md:!w-[250px] md:!min-h-[200px] md:!p-5 lg:!w-[250px] lg:!min-h-[200px] lg:!p-7"
+            className=" flex flex-col flex-nowrap items-center justify-center gap-7 glassy-screen !w-[380px] !min-h-[300px] !p-5 sm:!w-[3500px] sm:!min-h-[200px] sm:!p-4 md:!w-[350px] md:!min-h-[200px] md:!p-5 lg:!w-[550px] lg:!min-h-[200px] lg:!p-7"
           >
-            <p>Nombre: {project.nombre}</p>
-            <p>Detalle: {project.detalle}</p>
-            <p>
-              <a href="google.com">Link: {project.link}</a>
-            </p>
+            <label className=" w-[100%] text-justify ">
+              <p className="mb-[10px] mt-[20px]">Nombre:</p>
+              <p> {project.nombre}</p>
+            </label>
+            <label className=" w-[100%] text-justify ">
+              <p className="mb-[10px] mt-[20px]">Detalle:</p>
+              <p>{project.detalle}</p>
+            </label>
+
+            <label className=" w-[100%] text-ellipsis overflow-hidden ">
+              <p className="mb-[10px] mt-[20px]">Link:</p>
+              <a href={project.link} className="max-w-[30em] w-[100%] ">
+                {project.link}
+              </a>
+            </label>
             <div className="flex items-center justify-center">
               <button
                 className="glassy-icon m-3"
@@ -108,13 +118,18 @@ export default function Projects() {
                       method: "DELETE",
                     });
 
-                    const response = await fetch(Global.url + "/proyecto/listar");
+                    const response = await fetch(
+                      Global.url + "/proyecto/listar"
+                    );
                     const data = await response.json();
 
                     // Establecer los datos de educación en el estado
                     setProjectList(data.resultado);
                   } catch (error) {
-                    console.error("Error deleting project or updating list:", error);
+                    console.error(
+                      "Error deleting project or updating list:",
+                      error
+                    );
                   }
                 }}
               >
@@ -127,10 +142,16 @@ export default function Projects() {
       {/* Modal */}
       {isOpen && (
         <div className="fixed inset-0 w-full h-full flex items-center justify-center ">
-          <div className="absolute inset-0  bg-black opacity-75" onClick={closeModal}></div>
+          <div
+            className="absolute inset-0  bg-black opacity-75"
+            onClick={closeModal}
+          ></div>
           <div className=" p-8 rounded shadow-lg z-20 bg-modal">
             {/* Contenido de la modal */}
-            <form onSubmit={editarProyecto} className="w-[100%] flex flex-col flex-nowrap items-center gap-7">
+            <form
+              onSubmit={editarProyecto}
+              className="w-[100%] flex flex-col flex-nowrap items-center gap-7"
+            >
               <h3 className="text-xl font-semibold mb-4">Editar Proyecto</h3>
               <label className="max-w-[30em] w-[100%]" hidden>
                 id
@@ -166,13 +187,13 @@ export default function Projects() {
                   required
                 />
               </label>
-              <label className="max-w-[30em] w-[100%]">
+              <label className=" w-[100%]">
                 Link
                 <input
                   type="text"
                   id="linkEdit"
-                  name="contact-name"
-                  className="mt-3 px-5 py-4 bg-transparent border-2 border-solid border-neutGray-500 rounded-[5px] max-w-[30em] w-[100%] backdrop-blur-[5px]"
+                  name="linkEdit"
+                  className="mt-3 px-5 py-4 bg-transparent border-2 border-solid border-neutGray-500 rounded-[5px]  w-[100%] backdrop-blur-[5px]"
                   placeholder=""
                   required
                   defaultValue={data.link}
@@ -180,10 +201,16 @@ export default function Projects() {
               </label>
               {/* Botón para cerrar la modal */}
               <div className="flex items-center justify-center">
-                <button type="submit" className="mt-4  bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded glassy-icon m-3">
+                <button
+                  type="submit"
+                  className="mt-4  bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded glassy-icon m-3"
+                >
                   Editar
                 </button>
-                <button onClick={closeModal} className="mt-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded glassy-icon m-3">
+                <button
+                  onClick={closeModal}
+                  className="mt-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded glassy-icon m-3"
+                >
                   Cancelar
                 </button>
               </div>
